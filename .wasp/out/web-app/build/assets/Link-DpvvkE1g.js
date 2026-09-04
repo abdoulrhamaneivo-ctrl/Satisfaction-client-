fa64c6557aefba5c214ba9974c61ee2aa50658da
ABDOULRHAMANE IVO <abdoulrhamaneivo@gmail.com>
2026-09-04 05:16:16 +0200

    Rebuild client vite (wasp build avait vidé web-app/build)

---
+++ .wasp/out/web-app/build/assets/Link-DpvvkE1g.js
@@ -0,0 +1,1 @@
+import{d as e,aA as i,j as p,ai as u}from"./200--YmJ_ePf.js";function m({to:t,params:o,search:r,hash:n,...s}){const a=e.useMemo(()=>i(t,o,r,n),[t,o,r,n]);return p.jsx(u,{to:a,...s})}export{m as L};
